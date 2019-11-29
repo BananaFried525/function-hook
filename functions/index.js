6128448b7e7b9998c0f0830e07686bef24111135
newscontrol6 <newscontrol6@gmail.com>
2019-11-29 11:06:55 +0700

    add handlefunction

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,14 @@ const serviceWebHook = require('./src/webhook/webhook');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  ret ={
+    statusCode:200,
+    statusMsg:"Hello world",
+    message:req.headers
+  }
+
+  return res.send(ret);
 });
 
 exports.webhook = functions.https.onRequest(serviceWebHook.webhook);
---
+++ functions/src/service/handle-event.js
@@ -0,0 +1,4 @@
+module.exports = function(event) {
+  var xx = event;
+  return xx;
+}
