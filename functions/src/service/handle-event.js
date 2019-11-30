e35abb3c5064fdbf98a7d2ce27033292fbf308e3
BananaFried <newscontrol6@gmail.com>
2019-11-30 12:25:30 +0700

    reply text handle

--- functions/src/service/handle-event.js
+++ functions/src/service/handle-event.js
@@ -1,4 +1,22 @@
-module.exports = function(event) {
+module.exports = function (event) {
   var message = event;
-  return message;
+  var replyToken = event;
+  var result;
+
+  switch (message.type) {
+    case 'text': result = replyText(message);
+      break;
+    case 'location':
+      break;
+    default:
+      break;
+  }
+
+  return [replyToken, result];
+}
+
+function replyText(message) {
+  var reply = "";
+  reply = message.text;
+  return reply;
 }
