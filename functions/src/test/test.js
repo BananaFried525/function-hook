14a7e572a84894d51d51b2376ae714fe6518b9a6
newscontrol6 <newscontrol6@gmail.com>
2019-12-01 01:06:08 +0700

    edit test

--- functions/src/test/test.js
+++ functions/src/test/test.js
@@ -4,14 +4,12 @@ const db = require('../service/firestore');
 module.exports = (req, res) => {
   db.collection("Lov")
     .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.id, "=>", doc.data());
-      });
+    .then((snapshot) => {
+      res.send("ddd");
     })
     .catch(err => {
-      console.log("Error getting documents", err);
+      console.log("test");
+      res.send(err);
     });
 
-  res.send("pass");
 };
