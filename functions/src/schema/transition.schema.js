b547a3dc7e7056da3823812aad0532806b5562c5
newscontrol6 <newscontrol6@gmail.com>
2019-12-07 23:50:43 +0700

    add transition

--- functions/src/schema/transition.schema.js
+++ functions/src/schema/transition.schema.js
@@ -1,9 +1,9 @@
 const SchemaObject = require("schema-object");
 const NotEmptyString = { type: String, minLength: 1 };
 const transitionSchema = new SchemaObject({
-  //
-  //
-  //
+  userId: { type: String, require },
+  timestamp: { type: Date },
+  fnType: { type: String, require },
 });
 
-module.exports.User = transitionSchema;
+module.exports = transitionSchema;
--- functions/src/service/handle-event.js
+++ functions/src/service/handle-event.js
@@ -1,40 +1,91 @@
-module.exports.handleEvent = function (event) {
+/* eslint-disable promise/always-return */
+const db = require("./firestore");
+const transition = require("../schema/transition.schema");
+
+module.exports.handleEvent = function(event) {
   var message = event.message;
   var replyToken = event.replyToken;
   var result;
-
-  switch (message.type) {
-    case 'text': result = replyText(message);
-      console.info('text message detected');
-      break;
-    case 'location':
-      break;
-    case 'postback': result = postbackHandle(message);
-      console.info('');
-      break;
-    default:
-      break;
+  if (event.type === "postback") {
+    result = postbackHandle(event);
+  } else {
+    switch (message.type) {
+      case "text":
+        result = replyText(message);
+        console.info("text message detected");
+        break;
+      default:
+        result = {type:'text',text:'ไม่สามารถค้นหาคำสั่งนี้พบ'}
+        break;
+    }
   }
 
   return [replyToken, result];
-}
+};
 
 function replyText(message) {
   var reply = "";
   reply = {
-    type:'text',
-    text:message.text
+    type: "text",
+    text: message.text
   };
 
   return reply;
 }
 
-function postbackHandle(message){
-
-  reply = {
-    type:'text',
-    text:message.text
+function postbackHandle(event) {
+  var reply = {
+    type: "text",
+    text: event.postback.data
   };
+  var fn = event.postback.data;
+
+  var userId = event.source.userId;
+  var fnType = fn;
+  var timestamp = new Date();
+
+  switch (fn) {
+    case "bus_direction":
+      var data = {
+        userId,
+        fnType,
+        timestamp
+      };
+      var validate = new transition(data);
+      if (validate.isErrors()) {
+        console.error(validate.getErrors());
+      }
+      console.log(JSON.stringify(data))
+      var trans = db.collection("transitions").doc();
+      var userTrans = trans.set({ data })
+        .then(() => {
+          console.info("Put to firestore");
+        })
+        .catch(err => {
+          console.err(err);
+          return;
+        });
+
+      reply = {
+        type: "text",
+        text: "กรุณาเลือกสถานที่ต้นทางด้วยครับ",
+        quickReply: {
+          items: [
+            {
+              type: "action",
+              action: {
+                type: "location",
+                label: "Send location"
+              }
+            }
+          ]
+        }
+      };
+      break;
+
+    default:
+      break;
+  }
 
   return reply;
 }
