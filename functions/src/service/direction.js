da8afc75483b9e6f23c519fa04153d352ee500e8
BananaFried <newscontrol6@gmail.com>
2019-12-14 00:11:02 +0700

    add fn nearbyseach

--- functions/src/service/direction.js
+++ functions/src/service/direction.js
@@ -3,6 +3,7 @@ const configGoogle = require('../config/config.json')['google'];
 
 
 exports.findDirection = function () {
+  // https://maps.googleapis.com/maps/api/directions/json?departure_time=now&mode=transit&origin=มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ แขวง วงศ์สว่าง เขตบางซื่อ กรุงเทพมหานคร 10800&destination=อนุสาวรีย์ชัยสมรภูมิ ถนน พหลโยธิน แขวง ถนนพญาไท เขตราชเทวี กรุงเทพมหานคร 10400&key=AIzaSyAAm6Jci_ckEgvGkb98Q15R1h8RtIsjt_8
   var option ={
 
   }
--- functions/src/service/handle-event.js
+++ functions/src/service/handle-event.js
@@ -1,11 +1,13 @@
 /* eslint-disable promise/always-return */
 const db = require("./firestore");
 const transition = require("../schema/transition.schema");
+const googleApi = require("./google-api");
 
-module.exports.handleEvent = function(event) {
+module.exports.handleEvent = function (event) {
   var message = event.message;
   var replyToken = event.replyToken;
   var result;
+
   if (event.type === "postback") {
     result = postbackHandle(event);
   } else {
@@ -14,8 +16,23 @@ module.exports.handleEvent = function(event) {
         result = replyText(message);
         console.info("text message detected");
         break;
+      case 'location':
+        var user_locate = message.latitude+','+message.longitude;
+        googleApi.nearBySearch(user_locate)
+          .then((res)=>{
+            result = { type: 'text', text: JSON.stringify(res.data)};
+          }).catch((err)=>{
+            result = { type: 'text', text: JSON.stringify(err.message)};
+          }); // มัน return ก่อน+++
+
+        // if(res.status){
+        //   result = { type: 'text', text: JSON.stringify(res.data) };
+        // }else{
+        //   result = { type: 'text', text: JSON.stringify(res.message) };
+        // }
+        break;
       default:
-        result = {type:'text',text:'ไม่สามารถค้นหาคำสั่งนี้พบ'}
+        result = { type: 'text', text: 'ไม่สามารถค้นหาคำสั่งนี้พบ' }
         break;
     }
   }
@@ -38,24 +55,24 @@ function postbackHandle(event) {
     type: "text",
     text: event.postback.data
   };
-  var fn = event.postback.data;
 
   var userId = event.source.userId;
-  var fnType = fn;
+  var fnType = event.postback.data;
   var timestamp = new Date();
 
-  switch (fn) {
+  var data = {
+    userId,
+    fnType,
+    timestamp
+  };
+
+  var validate = new transition(data);
+  if (validate.isErrors()) {
+    console.error(validate.getErrors());
+  }
+  switch (fnType) {
     case "bus_direction":
-      var data = {
-        userId,
-        fnType,
-        timestamp
-      };
-      var validate = new transition(data);
-      if (validate.isErrors()) {
-        console.error(validate.getErrors());
-      }
-      console.log(JSON.stringify(data))
+
       var trans = db.collection("transitions").doc();
       var userTrans = trans.set({ data })
         .then(() => {
@@ -82,7 +99,23 @@ function postbackHandle(event) {
         }
       };
       break;
-
+    case 'richmenu_hotel':
+      reply = {
+        type: "text",
+        text: "กรุณาเลือกสถานที่ปัจจุบัน",
+        quickReply: {
+          items: [
+            {
+              type: "action",
+              action: {
+                type: "location",
+                label: "เลือกสถานที่"
+              }
+            }
+          ]
+        }
+      };
+      break;
     default:
       break;
   }
