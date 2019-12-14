3f83d3bae4b5bad60a3c67e1e43bb21735ad779b
Weerawat tanta <riskywerawat@Weerawats-MacBook-Pro.local>
2019-12-14 14:02:58 +0700

    clear test

--- functions/src/test/test.js
+++ functions/src/test/test.js
@@ -7,25 +7,23 @@ const rest = require("../service/restaurant");
 /*********************************** import undercore js ***********************************/
 const _ = require("underscore");
 module.exports = (req, res) => {
-  const result = rest.findRestaurant();
-  Promise.all(result).then(result => {
-    console.log(result);
-  });
-  // Object_req_body = {
-  //   LovType: "LovType",
-  //   LovName: "",
-  //   Createby: "",
-  //   Updateby: "",
-  //   CreatedAt: new Date(),
-  //   UpdatedAt: new Date()a
-  // };
-  // req_query = {
-  //   LovName: "test02"
-  // };
-  //deleteprovicebydocId(res, "asdasdasqwe");
-  // getprovice(res, "LovName", "==", "test02");
-  // createprovice(res, Object_req_body);   test 03/12/2562 00:20
-  // getallprovice(res); //test 02/12/2562 22:40
-  // getproviceOptions(res, "LovName", "==", "test02"); //test 04/12/2562
+  res.status(200).send("hello");
 };
+// Object_req_body = {
+//   LovType: "LovType",
+//   LovName: "",
+//   Createby: "",
+//   Updateby: "",
+//   CreatedAt: new Date(),
+//   UpdatedAt: new Date()a
+// };
+// req_query = {
+//   LovName: "test02"
+// };
+//deleteprovicebydocId(res, "asdasdasqwe");
+// getprovice(res, "LovName", "==", "test02");
+// createprovice(res, Object_req_body);   test 03/12/2562 00:20
+// getallprovice(res); //test 02/12/2562 22:40
+// getproviceOptions(res, "LovName", "==", "test02"); //test 04/12/2562
+
 /////////////////////////---getallprovice---/////////////////////////////
