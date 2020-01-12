1982c3284f28443c573a58ef017dba8a2be4752f
BananaFried525 <newscontrol6@gmail.com>
2020-01-13 00:15:33 +0700

    fix error path

--- functions/src/test/test.js
+++ functions/src/test/test.js
@@ -1,10 +1,8 @@
 /* eslint-disable promise/always- */
 /*********************************** import Database - Firestore ***********************************/
 const db = require("../services/firestore");
-/*********************************** import schema validator ***********************************/
-const lov = require("../schema/lov.schema")["lovSchema"];
 
-const googleapi = require("../service/google-api");
+const googleapi = require("../services/google-api");
 /*********************************** import undercore js ***********************************/
 const _ = require("underscore");
 
