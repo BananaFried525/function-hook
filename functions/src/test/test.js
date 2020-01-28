6d9253424d7ee6ef325c9fbe49e248a6a23a3bbc
BananaFried525 <newscontrol6@gmail.com>
2020-01-28 18:34:37 +0700

    edit test

--- functions/src/test/test.js
+++ functions/src/test/test.js
@@ -7,7 +7,27 @@ const googleapi = require("../services/google-api");
 /*********************************** import undercore js ***********************************/
 const _ = require("underscore");
 
-module.exports = (req, res) => {
-  // eslint-disable-next-line promise/catch-or-return
-  res.send("abcd");
-};
+// module.exports = (req, res) => {
+
+//   // eslint-disable-next-line promise/catch-or-return
+//   res.send("abcd");
+// };
+
+/**
+ * ! เอาไว้ทดสอบ Android 
+ */
+let express = require('express');
+let router = express.Router();
+
+router.get('/v1',(req,res)=>{
+  res.send('v1');
+});
+
+/**
+ * @package Other path
+ */
+router.get('**',(req,res)=>{
+  res.send('Not found');
+});
+
+module.exports = router;
